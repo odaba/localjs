2385289e40971bd1acbdf47d2629a87cdc5cb389
support <support@localjs.org>
2010-10-14 14:46:51 +0000

    synchronize merge

--- LocalJS_Release/localjs/localjs_file.js
+++ LocalJS_Release/localjs/localjs_file.js
@@ -131,7 +131,7 @@
 			}
 			catch (e)
 			{
-				default_value = 0;
+				default_value = "";
 			}
 
 			var str = newBuffer(4096);
@@ -159,7 +159,7 @@
 		// file exist
 		localjs_file.fileExists = function(filename)
 		{
-			return fnToBoolean(fnGetFso().FileExists(filename))
+			return fnToBoolean(fnGetFso().FileExists(filename));
 		}
 
 		// folder exists
