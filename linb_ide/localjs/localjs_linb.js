1d1be05ab8df6245e2bda95d38ed5b3584c6f657
support <support@localjs.org>
2010-11-10 12:45:30 +0000

    initially put linb ide to localjs repository

---
+++ linb_ide/localjs/localjs_linb.js
@@ -0,0 +1,57 @@
+/*
+	This code is part of LocalJS Open source.
+
+	http://localjs.org/
+
+	It's free to use this code anywhere, provided that this declaration is included
+*/
+
+// This script initializes namespace LOCALJS.LINB
+(function ()
+{
+    if ("undefined" == typeof (LOCALJS))
+        LOCALJS = {};
+
+    var localjs_namespace = LOCALJS;
+
+	localjs_namespace.LINB = localjs_namespace.LINB || {};
+
+	var localjs_linb = localjs_namespace.LINB,
+		localjs_file = LOCALJS.FILE;
+
+	localjs_linb.saveTemplateFile = function(template_filename, template_paras, result_file_fullname)
+	{
+		var buildPath = localjs_file.buildPath,
+			exeFolder = localjs_file.getExeFolder(),
+			template_path = buildPath(buildPath(exeFolder, 'template'), template_filename),
+			template_content = localjs_file.readFileUTF8(template_path);
+
+		if (false === template_content)
+			return template_content;
+
+		if (template_paras.clsName)
+			template_content = template_content.replace(/\{clsName\}/g, template_paras.clsName);
+		if (template_paras.theme)
+			template_content = template_content.replace(/\{theme\}/g, template_paras.theme);
+		if (template_paras.lang)
+			template_content = template_content.replace(/\{lang\}/g, template_paras.lang);
+		if (template_paras.content)
+			template_content = template_content.replace(/\{content\}/g, template_paras.content);
+
+		try
+		{
+			localjs_file.writeFileUTF8(result_file_fullname, template_content);
+		}
+		catch (e)
+		{
+			return false;
+		}
+
+		return true;
+	};
+	
+	localjs_linb.isLocal = function()
+	{
+		return localjs_file.isFileUrl(localjs_file.normalizeUrl());
+	};
+})();
